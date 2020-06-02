5e3aecab0ceb2f84c09673f772fb9ce54cad3d49
12553638 <54832510+12553638@users.noreply.github.com>
2020-06-02 13:29:24 +1000

    Collision avoidance
    testing and fixing errors

--- Code/Environment.m
+++ Code/Environment.m
@@ -1,5 +1,5 @@
 
-function partMesh = Environment(object, xOffset, yOffset, zOffset)
+function [partMesh, f, v, faceNormals] = Environment(object, xOffset, yOffset, zOffset)
     [f,v,data] = plyread(object,'tri');
 
     % Scale the colours to be 0-to-1 (they are originally 0-to-255
@@ -9,4 +9,13 @@ function partMesh = Environment(object, xOffset, yOffset, zOffset)
     partMesh = trisurf(f,v(:,1) + xOffset,v(:,2) + yOffset, v(:,3) + zOffset ...
       ,'FaceVertexCData',vertexColours,'EdgeColor','interp','EdgeLighting','flat');
    
+  faceNormals = zeros(size(f,1),3);
+    for faceIndex = 1:size(f,1)
+        v1 = v(f(faceIndex,1)',:);
+        v2 = v(f(faceIndex,2)',:);
+        v3 = v(f(faceIndex,3)',:);
+        faceNormals(faceIndex,:) = unit(cross(v2-v1,v3-v1));
+    end
+  
+  
 end
